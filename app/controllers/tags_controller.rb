4aba370b180a48ff2f29681fef1ff0c374bfa722
CKHarrison <hirohumi1882@gmail.com>
2020-06-26 19:12:53 -0700

    Implemented deletion of tags

--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -6,4 +6,12 @@ class TagsController < ApplicationController
   def show
     @tag = Tag.find(params[:id])
   end
+
+  def destroy
+    @tag = Tag.find(params[:id])
+    tag_name = @tag.name
+    @tag.destroy
+    flash.notice = "#{tag_name} deleted"
+    redirect_to tags_path
+  end
 end
